868cca065b6c0341b9de7f12941e9430d06f29eb
YaraNasrallah <73755557+YaraNasrallah@users.noreply.github.com>
2021-07-17 13:15:26 -0400

    WIP: Style Walkthrough #6 (#10)
    * Creating Branch (style walkthrough)
    
    * Added new features to walkthrough to match figma design. Facing issues with 1- Third walkthrough slide image positioning 2- custom font is not working 3- how to add status bar above button
    
    * fix img centering
    
    * move fonts
    
    Co-authored-by: Lucy Hao <hao.lucyy@gmail.com> @lhao03

--- lib/screens/new-user/walkthrough_screens/walkthrough_template.dart
+++ lib/screens/new-user/walkthrough_screens/walkthrough_template.dart
@@ -1,7 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:frontend_m2m/model/m2m_user.dart';
 import 'package:frontend_m2m/screens/bottom-nav/bottom-nav.dart';
-import 'package:frontend_m2m/screens/wrapper.dart';
 import 'package:provider/provider.dart';
 
 class Walkthrough extends StatelessWidget {
@@ -10,13 +11,15 @@ class Walkthrough extends StatelessWidget {
   final String blurb;
   final Function action;
   final String header;
+  final Alignment imgAlignment;
 
   Walkthrough(
       {required this.imgPath,
       required this.buttonStr,
       required this.blurb,
       required this.action,
-      required this.header});
+      required this.header,
+      this.imgAlignment = Alignment.centerLeft});
 
   @override
   Widget build(BuildContext context) {
@@ -25,29 +28,64 @@ class Walkthrough extends StatelessWidget {
       return Home();
     } else {
       String walkthroughImgsFolder = "assets/new_user_walkthrough/";
+
       return Scaffold(
         backgroundColor: Colors.white,
         body: Container(
-          padding: EdgeInsets.symmetric(vertical: 50.0),
           child: Column(
             children: <Widget>[
-              Image(
-                height: MediaQuery.of(context).size.height / 2,
-                image:
-                    AssetImage(walkthroughImgsFolder + this.imgPath + ".png"),
+              Align(
+                alignment: this.imgAlignment,
+                child: Image(
+                  height: MediaQuery.of(context).size.height / 2,
+                  width: 250,
+                  image:
+                      AssetImage(walkthroughImgsFolder + this.imgPath + ".png"),
+                ),
               ),
-              Text(
-                this.header,
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(
+                    this.header,
+                    style: TextStyle(fontFamily: 'Rubik'),
+                  ),
+                ),
               ),
-              Text(
-                this.blurb,
+              Center(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(left: 40, right: 40, bottom: 60),
+                  child: Text(
+                    this.blurb,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontFamily: 'Rubik'),
+                  ),
+                ),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  this.action();
-                },
-                child: Text(this.buttonStr),
-              )
+              SizedBox(
+                width: 270,
+                height: 39,
+                child: ElevatedButton(
+                  onPressed: () {
+                    this.action();
+                  },
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    )),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue.shade600),
+                  ),
+                  child: Text(
+                    this.buttonStr,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
